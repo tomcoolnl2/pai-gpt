9a29b2e7b5208709918e8ae6cf54bfee336ba8ca
tomcoolnl2 <tomcoolnl@gmail.com>
2023-12-18 12:44:19 +0100

    feat(1993): added routing for chat pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<div>
 			<Head>
-				<title>Next JS ChatGPT Starter</title>
+			<title>PAI - GPT</title>
 			</Head>
 			<h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
 		</div>
