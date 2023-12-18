c337d64650349c658f99777e196e37e11d44086e
tomcoolnl2 <tomcoolnl@gmail.com>
2023-12-18 15:19:59 +0100

    feat(2004): redirect to /chat

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import { getSession } from '@auth0/nextjs-auth0';
 
 export default function Home() {
 	const { isLoading, error, user } = useUser();
@@ -18,7 +19,14 @@ export default function Home() {
 			<Head>
 				<title>PAI - GPT</title>
 			</Head>
-			<div className="flex min-h-screen w-full items-center justify-center bg-gray-800">
+			<div className="flex min-h-screen w-full flex-col items-center justify-center bg-gray-800">
+				<figure className="flex max-w-lg flex-col items-center">
+					<img src="/logo.svg" width={100} />
+					<figcaption className="mt-2 text-center text-sm text-gray-500 dark:text-gray-400">
+						PAI - GPT
+					</figcaption>
+				</figure>
+				<hr className="mx-auto my-4 h-1 w-48 rounded border-0 bg-gray-100 dark:bg-gray-700 md:my-10" />
 				{!!user && (
 					<Link href="/api/auth/logout" className="text-white">
 						Logout
@@ -36,3 +44,14 @@ export default function Home() {
 		</>
 	);
 }
+
+export const getServerSideProps = async ({ req, res }) => {
+	const session = await getSession(req, res);
+	return !session
+		? { props: {} }
+		: {
+				redirect: {
+					destination: '/chat',
+				},
+		  };
+};
