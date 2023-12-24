513f271ae6faa9e2ae80ed29cba67573175d56e3
Tom Cool <tomcoolnl@gmail.com>
2023-12-24 11:24:54 +0100

    style: added styling for the sidebar (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Head from 'next/head';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { ConversationProvider } from 'context/conversation';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.css';
+
+config.autoAddCss = false;
 
 const App = ({ Component, pageProps }) => {
 	return (
