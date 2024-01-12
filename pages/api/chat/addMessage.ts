f3f60e78e61c6e4c54ca1fe8540034363b3cb49f
tomcoolnl2 <tomcoolnl@gmail.com>
2024-01-12 16:03:36 +0100

    feat(2046): Add validation to addMessageToChat endpoint

--- pages/api/chat/addMessage.ts
+++ pages/api/chat/addMessage.ts
@@ -1,12 +1,10 @@
-// will be running in a Node environment
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Filter, FindOneAndUpdateOptions, UpdateFilter, ObjectId } from 'mongodb';
 import { getSession } from '@auth0/nextjs-auth0';
+import { Filter, FindOneAndUpdateOptions, UpdateFilter, ObjectId } from 'mongodb';
 import clientPromise from 'lib/mongodb';
 
 export default async function addMessage(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	try {
-		//
 		const { user } = await getSession(req, res);
 		const client = await clientPromise;
 		const db = client.db('pai-gpt');
@@ -16,8 +14,37 @@ export default async function addMessage(req: NextApiRequest, res: NextApiRespon
 			payload: { content, role },
 		} = req.body;
 
+		let objectId: ObjectId;
+		try {
+			objectId = new ObjectId(conversationId);
+		} catch (e) {
+			res.status(422).json({
+				message: 'Invalid conversation ID',
+			});
+			return;
+		}
+
+		if (
+			!content ||
+			typeof content !== 'string' ||
+			(role === 'user' && content.length > 200) ||
+			(role === 'assistant' && content.length > 100000)
+		) {
+			res.status(422).json({
+				message: 'Invalid message content or length',
+			});
+			return;
+		}
+
+		if (!['user', 'assistant'].includes(role)) {
+			res.status(422).json({
+				message: 'Role must be either "assistant" or "user"',
+			});
+			return;
+		}
+
 		const filter: Filter<Document> = {
-			_id: new ObjectId(conversationId),
+			_id: objectId,
 			userId: user.sub,
 		};
 
@@ -38,7 +65,7 @@ export default async function addMessage(req: NextApiRequest, res: NextApiRespon
 
 		const conversationResult = await db
 			.collection('conversations')
-			.findOneAndUpdate(filter, update as any, options);
+			.findOneAndUpdate(filter, update as unknown, options);
 
 		res.status(200).json({
 			conversation: {
@@ -46,9 +73,10 @@ export default async function addMessage(req: NextApiRequest, res: NextApiRespon
 				_id: conversationResult._id.toString(),
 			},
 		});
-	} catch (e) {
+	} catch (error) {
+		console.error('Error adding message:', error);
 		res.status(500).json({
-			message: 'An error occured when adding a message to a chat.',
+			message: 'An error occurred when adding a message to a conversation.',
 		});
 	}
 }
